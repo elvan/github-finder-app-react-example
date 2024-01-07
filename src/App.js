68495eff9776cb7974277f168880cb95ba77baa6
Elvan Hidayat <elvanhd@gmail.com>
2024-01-07 13:17:52 +0700

    context and reducer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
+import { GithubProvider } from './context/github/GithubContext';
 import About from './pages/About';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
 
-        <main className='container mx-auto px-3 pb-12'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/about' component={About} />
-            <Route path='/notfound' component={NotFound} />
-            <Route component={NotFound} />
-          </Switch>
-        </main>
+          <main className='container mx-auto px-3 pb-12'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/notfound' component={NotFound} />
+              <Route component={NotFound} />
+            </Switch>
+          </main>
 
-        <Footer />
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
