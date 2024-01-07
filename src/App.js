554375237621e770b80ea9b4f71b08af8640656c
Elvan Hidayat <elvanhd@gmail.com>
2024-01-07 21:05:28 +0700

    user profile and repos

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Alert from './components/layout/Alert';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
+import { AlertProvider } from './context/alert/AlertContext';
 import { GithubProvider } from './context/github/GithubContext';
 import About from './pages/About';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import User from './pages/User';
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className='flex flex-col justify-between h-screen'>
-          <Navbar />
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
 
-          <main className='container mx-auto px-3 pb-12'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/about' component={About} />
-              <Route path='/notfound' component={NotFound} />
-              <Route component={NotFound} />
-            </Switch>
-          </main>
+            <main className='container mx-auto px-3 pb-12'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About} />
+                <Route path='/user/:login' component={User} />
+                <Route path='/notfound' component={NotFound} />
+                <Route component={NotFound} />
+              </Switch>
+            </main>
 
-          <Footer />
-        </div>
-      </Router>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
